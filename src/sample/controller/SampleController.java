cfefe006bf1f5c84da4f3f4229dcfaca90419b1b
mmce2349 <31513795+mmce2349@users.noreply.github.com>
2017-09-07 09:52:00 -0600

    Creation of Scanner

--- src/sample/controller/SampleController.java
+++ src/sample/controller/SampleController.java
@@ -1,10 +1,23 @@
 package sample.controller;
 
+import java.util.Scanner;
+
 public class SampleController 
 {
 	public void start()
 	{
-		System.out.println("Thisis a sample project to show the github and");
+		System.out.println("This is a sample project to show the github and");
 		System.out.println("eclipse process so I develop muscle memory :D");
+		
+		testScanner();
+	}
+	
+	private void testScanner()
+	{
+		Scanner firstScanner; //Declares a Scanner object.
+		firstScanner = new Scanner(System.in);//Instanciates a Scanner object
+		System.out.println("Please type your favorite food");
+		String answer = firstScanner.nextLine();//User types and computer gets it
+		System.out.println("Oh, you like to eat " + answer);
 	}
 }
