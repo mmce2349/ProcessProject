db373aba50b8f297430015042be623bbc64bdcb8
mmce2349 <31513795+mmce2349@users.noreply.github.com>
2017-09-11 08:23:45 -0600

    Added my PlayDohCircle class.

--- src/sample/controller/SampleRunner.java
+++ src/sample/controller/SampleRunner.java
@@ -4,7 +4,7 @@ public class SampleRunner
 {	
 	public static void main(String [] args)
 	{
-		SampleController myApp = new SampleController();
-		myApp.start();
+		SampleController myApp = new SampleController();// Equal signs is not math equal sign. = is assignment operator. One way only (Right to left). New is a keyword. 
+		myApp.start();// Constructor has same name as type. 
 	}
 }
